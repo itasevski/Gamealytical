1c3353c190f207c298d58e6510472e3abeb46bf4
Ivo Tasevski <ivo_t@live.com>
2022-02-16 19:05:51 +0100

    Project setup.

---
+++ src/test/java/mk/ukim/finki/gamealytical/GamealyticalApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.gamealytical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamealyticalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
